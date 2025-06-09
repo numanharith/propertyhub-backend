4551dfccd51c409aefb195b78b3864d831baacb2
numan <numan.h.99@gmail.com>
2025-06-09 23:17:32 +0800

    renamed location column to location_address

--- src/main/java/com/propertyhub/model/Property.java
+++ src/main/java/com/propertyhub/model/Property.java
@@ -37,7 +37,7 @@ public class Property {
     @Column(nullable = false, precision = 18, scale = 2)
     private BigDecimal price;
 
-    @Column(name = "location", nullable = false)
+    @Column(name = "location_address", nullable = false)
     private String location; // Full address
 
     @Column(name = "location_lat", precision = 9, scale = 6)
